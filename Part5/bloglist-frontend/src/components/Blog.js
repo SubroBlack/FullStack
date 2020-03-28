3cb5eabcfacdcde95bcec26b7353ee3c67f87d7a
SubroBlack <dahalsubodh8@gmail.com>
2020-03-28 19:05:59 +0200

    Ex 5.7 DONE, clocking on the title reveals and hides the details of the blog

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    <b>{blog.title}</b> - {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const showBlog = () => {
+    if (!showDetails) {
+      return (
+        <div onClick={toggleShowDetails}>
+          <b>{blog.title}</b> - {blog.author}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div onClick={toggleShowDetails}>
+            <b>{blog.title}</b>
+          </div>
+          <p>By {blog.author}</p>
+          <p>Found at: {blog.url}</p>
+          <p>with {blog.likes} likes</p>
+        </div>
+      );
+    }
+  };
+
+  return <div>{showBlog()}</div>;
+};
 
 export default Blog;
